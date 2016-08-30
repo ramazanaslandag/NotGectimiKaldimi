4d08ddb27094594ae90cb3fc0ba840bc3629d1e3
TyLeR <TyLeR@DESKTOP-I4AAU3N>
2016-08-30 20:25:42 +0300

    Add project files.

---
+++ VizeFinalGectimiKaldimi/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VizeFinalGectimiKaldimi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //vize final yoluyla ortalama bulunacak:
+            //final 50nin altındaysa kalır ayrıca final 50nin üstündeyse ve ortalama 50nin altındaysa yine kalır ☺
+
+            double vize, final, ortalama;
+            Console.Write("Vize Notu:");
+            vize = double.Parse(Console.ReadLine());
+            Console.Write("Final Notu:");
+            final = Convert.ToDouble(Console.ReadLine());
+
+            if (final <50)
+            {
+                Console.WriteLine("Finalden kaldınız");
+                
+            }
+            else//final 50 ve üzerindeyse
+            {
+                ortalama = vize * .4 + final * .6;
+                if (ortalama>=50)
+                {
+                    Console.WriteLine("Tebrikler {0} ortalama ile geçtiniz.",ortalama);
+                }
+                else
+                {
+                    Console.WriteLine("{0} ortalamayla kaldınız",ortalama);
+                    
+                }
+                
+            }
+            Console.ReadLine();
+        }
+        
+    }
+}
